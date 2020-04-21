dbe3466267852180aed5f678757608b4fd5d1213
hanchen8 <hanchen8@illinois.edu>
2020-04-21 17:29:35 -0500

    construct files structure

---
+++ include/fpgakrnl/Dialect.h
@@ -0,0 +1,18 @@
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/Function.h"
+#include "mlir/Interfaces/SideEffects.h"
+
+namespace mlir {
+namespace fpgakrnl {
+
+class FpgaKrnlDialect : public mlir::Dialect {
+    explicit FpgaKrnlDialect(mlir::MLIRContext *ctx);
+    static llvm::StringRef getDialectNamespace() {
+        return "fpgakrnl";
+    }
+};
+
+#define GET_OP_CLASSES
+#include "toy/Ops.h.inc"
+}
+}
