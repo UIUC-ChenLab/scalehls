fe2cc4fdbab9cf9605c32053da0b450109b6d83a
Hanchen Ye <hanchenye@gmail.com>
2020-08-27 23:27:33 -0500

    [EmitHLSCpp] update code structure; add two visitors

--- lib/EmitHLSCpp/EmitHLSCpp.cpp
+++ lib/EmitHLSCpp/EmitHLSCpp.cpp
@@ -2,7 +2,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "EmitHLSCpp.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/SCF/SCF.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/Function.h"
 #include "mlir/IR/Module.h"
 #include "mlir/IR/StandardTypes.h"
@@ -10,10 +12,113 @@
 #include "llvm/ADT/StringSet.h"
 #include "llvm/Support/raw_ostream.h"
 
+#include "EmitHLSCpp.h"
+
 using namespace mlir;
 
 //===----------------------------------------------------------------------===//
-// Utilities
+// Visitors
+//
+// These classes should (?) be factored out from here, and can be inherited by
+// different backend/emitter (e.g. HLS Cpp for Vivado, HLS Cpp for Intel FPGAs,
+// OpenCL, etc.).
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// StmtVisitor is a visitor for statement nodes.
+template <typename ConcreteType, typename ResultType = void,
+          typename... ExtraArgs>
+class StmtVisitor {
+public:
+  ResultType dispatchStmtVisitor(Operation *op, ExtraArgs... args) {
+    auto *thisCast = static_cast<ConcreteType *>(this);
+    return TypeSwitch<Operation *, ResultType>(op)
+        .template Case<scf::IfOp>([&](auto opNode) -> ResultType {
+          return thisCast->visitStmt(opNode, args...);
+        })
+        .Default([&](auto expr) -> ResultType {
+          return thisCast->visitInvalidStmt(op, args...);
+        });
+  }
+
+  /// This callback is invoked on any non-statement operations.
+  ResultType visitInvalidStmt(Operation *op, ExtraArgs... args) {
+    op->emitOpError("Unknown statement.");
+    abort();
+  }
+
+  /// This callback is invoked on any statement operations that are not handled
+  /// by the concrete visitor.
+  ResultType visitUnhandledStmt(Operation *op, ExtraArgs... args) {
+    return ResultType();
+  }
+
+#define HANDLE(OPTYPE)                                                         \
+  ResultType visitStmt(OPTYPE op, ExtraArgs... args) {                         \
+    return static_cast<ConcreteType *>(this)->visitUnhandledStmt(op, args...); \
+  }
+
+  HANDLE(scf::IfOp);
+#undef HANDLE
+};
+} // namespace
+
+namespace {
+/// ExprVisitor is a visitor for expression nodes.
+template <typename ConcreteType, typename ResultType = void,
+          typename... ExtraArgs>
+class ExprVisitor {
+public:
+  ResultType dispatchExprVisitor(Operation *op, ExtraArgs... args) {
+    auto *thisCast = static_cast<ConcreteType *>(this);
+    return TypeSwitch<Operation *, ResultType>(op)
+        .template Case<AddIOp>([&](auto expr) -> ResultType {
+          return thisCast->visitExpr(expr, args...);
+        })
+        .Default([&](auto expr) -> ResultType {
+          return thisCast->visitInvalidExpr(op, args...);
+        });
+  }
+
+  /// This callback is invoked on any non-expression operations.
+  ResultType visitInvalidExpr(Operation *op, ExtraArgs... args) {
+    op->emitOpError("Unknown expression.");
+    abort();
+  }
+
+  /// This callback is invoked on any expression operations that are not handled
+  /// by the concrete visitor.
+  ResultType visitUnhandledExpr(Operation *op, ExtraArgs... args) {
+    return ResultType();
+  }
+
+  /// This fallback is invoked on any unary expr that isn't explicitly handled.
+  /// The default implementation delegates to the unhandled expression fallback.
+  ResultType visitUnaryExpr(Operation *op, ExtraArgs... args) {
+    return static_cast<ConcreteType *>(this)->visitUnhandledExpr(op, args...);
+  }
+
+  /// This fallback is invoked on any binary expr that isn't explicitly handled.
+  /// The default implementation delegates to the unhandled expression fallback.
+  ResultType visitBinaryExpr(Operation *op, ExtraArgs... args) {
+    return static_cast<ConcreteType *>(this)->visitUnhandledExpr(op, args...);
+  }
+
+#define HANDLE(OPTYPE, OPKIND)                                                 \
+  ResultType visitExpr(OPTYPE op, ExtraArgs... args) {                         \
+    return static_cast<ConcreteType *>(this)->visit##OPKIND##Expr(op,          \
+                                                                  args...);    \
+  }
+
+  HANDLE(AddIOp, Binary);
+#undef HANDLE
+};
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// HLSCppEmitter Base Classes
+//
+// These classes should (?) be factored out from here somehow.
 //===----------------------------------------------------------------------===//
 
 namespace {
@@ -70,50 +175,100 @@ private:
 } // namespace
 
 //===----------------------------------------------------------------------===//
-// FuncEmitter
+// ExprEmitter Class
 //===----------------------------------------------------------------------===//
 
 namespace {
-class FuncEmitter : public HLSCppEmitterBase {
+class ExprEmitter : public ExprVisitor<ExprEmitter> {
 public:
-  explicit FuncEmitter(HLSCppEmitterState &state) : HLSCppEmitterBase(state) {}
-
-  void emitFunc(FuncOp func);
+private:
 };
 } // namespace
 
-void FuncEmitter::emitFunc(FuncOp func) { os << "new function\n"; };
-
 //===----------------------------------------------------------------------===//
-// HLSCppEmitter
+// StmtEmitter Class
 //===----------------------------------------------------------------------===//
 
 namespace {
-class HLSCppEmitter : public HLSCppEmitterBase {
+class StmtEmitter : public ExprVisitor<StmtEmitter> {
 public:
-  explicit HLSCppEmitter(HLSCppEmitterState &state)
+private:
+};
+} // namespace
+
+//===----------------------------------------------------------------------===//
+// ModuleEmitter Class
+//===----------------------------------------------------------------------===//
+
+namespace {
+class ModuleEmitter : public HLSCppEmitterBase {
+public:
+  explicit ModuleEmitter(HLSCppEmitterState &state)
       : HLSCppEmitterBase(state) {}
 
-  void emitMLIRModule(ModuleOp module);
+  void emitOperation(Operation *op);
+  void emitFunc(FuncOp func);
+  void emitModule(ModuleOp module);
 };
 } // namespace
 
-void HLSCppEmitter::emitMLIRModule(ModuleOp module) {
+void ModuleEmitter::emitOperation(Operation *op) {
+  os << "new op:" << op->getName() << ";\n";
+};
+
+void ModuleEmitter::emitFunc(FuncOp func) {
+  os << "void " << func.getName() << " (";
+
+  // TODO: handle arguments.
+  os << ") {\n";
+  addIndent();
+
+  // TODO: daclarations.
+
+  // TODO: handle all operations.
+  if (func.getBlocks().size() != 1) {
+    func.emitError("More than one blocks in the current function.");
+  }
+  for (auto &op : func.front()) {
+    indent();
+    emitOperation(&op);
+  }
+  os << "}\n";
+};
+
+void ModuleEmitter::emitModule(ModuleOp module) {
+  os << R"XXX(
+//===------------------------------------------------------------*- C++ -*-===//
+//
+// Automatically generated file for High-level Synthesis (HLS).
+//
+//===----------------------------------------------------------------------===//
+
+#include <ap_axi_sdata.h>
+#include <ap_fixed.h>
+#include <ap_int.h>
+#include <hls_math.h>
+#include <hls_stream.h>
+#include <math.h>
+#include <stdint.h>
+
+)XXX";
+
   for (auto &op : *module.getBody()) {
     if (auto func = dyn_cast<FuncOp>(op))
-      FuncEmitter(state).emitFunc(func);
+      emitFunc(func);
     else if (!isa<ModuleTerminatorOp>(op))
-      op.emitError("unknown operation");
+      op.emitError("Unknown operation.");
   }
 }
 
 //===----------------------------------------------------------------------===//
-// hlsld-translate Entry
+// Entry of hlsld-translate
 //===----------------------------------------------------------------------===//
 
 static LogicalResult emitHLSCpp(ModuleOp module, llvm::raw_ostream &os) {
   HLSCppEmitterState state(os);
-  HLSCppEmitter(state).emitMLIRModule(module);
+  ModuleEmitter(state).emitModule(module);
   return failure(state.encounteredError);
 }
 
--- test/EmitHLSCpp/test_standard.mlir
+++ test/EmitHLSCpp/test_standard.mlir
@@ -1,6 +1,11 @@
 // RUN: hlsld-translate -emit-hlscpp %s | FileCheck %s
 
-// CHECK: new function
+// CHECK: void test_standard (
+// CHECK: ) {
 func @test_standard() {
-
+  
+  // CHECK: new op:std.constant;
+  %c1 = constant 1 : index
+  return
+// CHECK: }
 }
