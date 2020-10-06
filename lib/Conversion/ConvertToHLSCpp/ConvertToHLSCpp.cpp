a2934a09a91e38bd4f271d97e602f0342c7cdcd5
Hanchen Ye <hanchenye@gmail.com>
2020-10-05 23:52:43 -0500

    [ConvertToHLSCpp] support to recursively convert for loop body

--- lib/Conversion/ConvertToHLSCpp/ConvertToHLSCpp.cpp
+++ lib/Conversion/ConvertToHLSCpp/ConvertToHLSCpp.cpp
@@ -19,12 +19,68 @@ public:
 };
 } // namespace
 
+static void convertBlock(Block &block) {
+  for (auto &op : block) {
+    if (isa<ArrayOp>(op))
+      continue;
+    auto b = OpBuilder(&op);
+
+    // ArrayOp will be inserted after each ShapedType value from declaration
+    // or function signature.
+    for (auto operand : op.getOperands()) {
+      if (auto arrayType = operand.getType().dyn_cast<ShapedType>()) {
+        bool insertArrayOp = false;
+        if (operand.getKind() == Value::Kind::BlockArgument)
+          insertArrayOp = true;
+        else if (!isa<ArrayOp>(operand.getDefiningOp())) {
+          insertArrayOp = true;
+          if (!arrayType.hasStaticShape())
+            operand.getDefiningOp()->emitError(
+                "is unranked or has dynamic shape which is illegal.");
+        }
+
+        if (insertArrayOp) {
+          // Insert array operation and set attributes.
+          b.setInsertionPointAfterValue(operand);
+          auto arrayOp =
+              b.create<ArrayOp>(op.getLoc(), operand.getType(), operand);
+          operand.replaceAllUsesExcept(arrayOp.getResult(),
+                                       SmallPtrSet<Operation *, 1>{arrayOp});
+
+          // Set array pragma attributes, default array instance is ram_1p
+          // bram. Other attributes are not set here since they requires more
+          // analysis to be determined.
+          arrayOp.setAttr("interface", b.getBoolAttr(false));
+          arrayOp.setAttr("storage_type", b.getStringAttr("ram_1p"));
+          arrayOp.setAttr("storage_impl", b.getStringAttr("bram"));
+          arrayOp.setAttr("partition", b.getBoolAttr(false));
+        }
+      }
+    }
+
+    if (auto forOp = dyn_cast<AffineForOp>(op)) {
+      if (forOp.getLoopBody().getBlocks().size() != 1)
+        forOp.emitError("has zero or more than one basic blocks");
+
+      // Set loop pragma attributes.
+      forOp.setAttr("pipeline", b.getBoolAttr(false));
+      forOp.setAttr("pipeline_II", b.getUI32IntegerAttr(1));
+      forOp.setAttr("unroll_factor", b.getUI32IntegerAttr(1));
+
+      convertBlock(forOp.getLoopBody().front());
+    }
+  }
+}
+
 void ConvertToHLSCppPass::runOnOperation() {
   for (auto func : getOperation().getOps<FuncOp>()) {
-    if (func.getBlocks().size() != 1)
-      func.emitError("has zero or more than one basic blocks");
-
     auto b = OpBuilder(func);
+
+    if (func.getBlocks().size() != 1)
+      func.emitError("has zero or more than one basic blocks.");
+
+    // Set function pragma attributes.
+    func.setAttr("dataflow", b.getBoolAttr(false));
 
     // Insert AssignOp.
     if (auto returnOp = dyn_cast<ReturnOp>(func.front().getTerminator())) {
@@ -45,51 +101,8 @@ void ConvertToHLSCppPass::runOnOperation() {
     } else
       func.emitError("doesn't have a return as terminator.");
 
-    // Set function pragma attributes.
-    func.setAttr("dataflow", b.getBoolAttr(false));
-
-    for (auto &op : func.front()) {
-      if (auto forOp = dyn_cast<AffineForOp>(op)) {
-        if (forOp.getLoopBody().getBlocks().size() != 1)
-          forOp.emitError("has zero or more than one basic blocks");
-
-        // Set loop pragma attributes.
-        forOp.setAttr("pipeline", b.getBoolAttr(false));
-        forOp.setAttr("pipeline_II", b.getUI32IntegerAttr(1));
-        forOp.setAttr("unroll_factor", b.getUI32IntegerAttr(1));
-      }
-
-      for (auto operand : op.getOperands()) {
-        if (auto arrayType = operand.getType().dyn_cast<ShapedType>()) {
-          bool insertArrayOp = false;
-          if (operand.getKind() == Value::Kind::BlockArgument)
-            insertArrayOp = true;
-          else if (!isa<ArrayOp>(operand.getDefiningOp())) {
-            insertArrayOp = true;
-            if (!arrayType.hasStaticShape())
-              operand.getDefiningOp()->emitError(
-                  "is unranked or has dynamic shape which is illegal.");
-          }
-
-          if (insertArrayOp) {
-            // Insert array operation and set attributes.
-            b.setInsertionPointAfterValue(operand);
-            auto arrayOp =
-                b.create<ArrayOp>(op.getLoc(), operand.getType(), operand);
-            operand.replaceAllUsesExcept(arrayOp.getResult(),
-                                         SmallPtrSet<Operation *, 1>{arrayOp});
-
-            // Set array pragma attributes, default array instance is ram_1p
-            // bram. Other attributes are not set here since they requires more
-            // analysis to be determined.
-            arrayOp.setAttr("interface", b.getBoolAttr(false));
-            arrayOp.setAttr("storage_type", b.getStringAttr("ram_1p"));
-            arrayOp.setAttr("storage_impl", b.getStringAttr("bram"));
-            arrayOp.setAttr("partition", b.getBoolAttr(false));
-          }
-        }
-      }
-    }
+    // Recursively convert every for loop body blocks.
+    convertBlock(func.front());
   }
 }
 
