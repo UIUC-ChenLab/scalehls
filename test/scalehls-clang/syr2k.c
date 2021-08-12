8d98a6120b1ea01414b1a9104bc334a3d7655250
Hanchen Ye <hanchenye@gmail.com>
2021-08-11 20:49:50 -0500

    [scalehls-clang] Add several test cases

---
+++ test/scalehls-clang/syr2k.c
@@ -0,0 +1,15 @@
+// RUN: scalehls-clang %s | FileCheck %s
+
+// CHECK: func @syr2k(
+void syr2k(float alpha, float beta, float C[32][32], float A[32][32],
+           float B[32][32]) {
+  for (int i = 0; i < 32; i += 1) {
+    for (int j = 0; j < (i + 1); j += 1) {
+      C[i][j] *= beta;
+      for (int k = 0; k < 32; k += 1) {
+        float tmp = A[i][k] * B[j][k] + B[i][k] * A[j][k];
+        C[i][j] += alpha * tmp;
+      }
+    }
+  }
+}
---
+++ test/scalehls-clang/trmm.c
@@ -0,0 +1,13 @@
+// RUN: scalehls-clang %s | FileCheck %s
+
+// CHECK: func @trmm(
+void trmm(float alpha, float A[32][32], float B[32][32]) {
+  for (int i = 0; i < 32; i += 1) {
+    for (int j = 0; j < 32; j += 1) {
+      for (int k = 0; k < (i + 1); k += 1) {
+        B[i][j] += A[i][k] * B[k][j];
+      }
+      B[i][j] *= alpha;
+    }
+  }
+}
