fff1aded164b9f9ddf8219f42a3860cd377b73dd
Luke Jacobs <lukedj2@xacc-head-000-5.csl.illinois.edu>
2021-11-21 23:44:38 -0600

    emit to IP library experimental features complete

--- lib/Translation/EmitIPLibrary.cpp
+++ lib/Translation/EmitIPLibrary.cpp
@@ -22,34 +22,60 @@ LogicalResult scalehls::emitIPLibrary(ModuleOp module, llvm::raw_ostream &os) {
   // TODO: Somehow we need to find a way to get a filename argument into this scope. How can I add a filename option to the "emit-ip" cmd-line arg?
   
   std::string filename = "iplibrary.json";  // This will need to be set by the command line
-  std::string optimized_source, json_file_contents;
-  llvm::raw_string_ostream stream_to_str(optimized_source);
 
-  ScaleHLSEmitterState state(stream_to_str);
-  ModuleEmitter(state).emitModule(module);
+  // ===== Emit HLS code =====
+  // ScaleHLSEmitterState state(stream_to_str);
+  // ModuleEmitter(state).emitModule(module);
 
-  /* Build the JSON object representing the IP */
-  // llvm::json::Object ip_in_json;
-  // ip_in_json.insert({"source", optimized_source});
+  // ===== Emit optimized MLIR given to us by scalehls-opt =====
+  
+  llvm::json::Object ip_json;
+  ip_json["raw"] = llvm::json::Value("");  // Default raw (unoptimized) json entry
+  ip_json["source"] = llvm::json::Value("");  // Default source (optimized) json entry
+  
+  // Iterate through the MLIR functions in the MLIR module presented to us
+  // We assume there are two functions given, one having a "bypass = true" attribute
+  for (auto func : module.getOps<FuncOp>()) {
+    FuncOpAdaptor func_adaptor(func);
+    DictionaryAttr attr_dict = func_adaptor.getAttributes();
+    
+    // If this function contains "bypass = true", then emit it to the IP JSON under the "raw" field
+    Attribute bypass_attr = attr_dict.get("bypass");
+    if (bypass_attr != Attribute()) {
+      std::string bypass_func_serialized;
+      llvm::raw_string_ostream bypass_func_ss(bypass_func_serialized);
+      bypass_func_ss << func;  // Luke- I had using this string streaming idiom but it seems to be the only option
+      ip_json["raw"] = bypass_func_serialized;
+    } else {
+      std::string source_func_serialized;
+      llvm::raw_string_ostream source_func_ss(source_func_serialized);
+      source_func_ss << func;
+      ip_json["source"] = source_func_serialized;
+    }
+  }
 
-  /* JSON emission to string */
-  llvm::raw_string_ostream stream_to_json_str(json_file_contents);
-  llvm::json::OStream json_os(stream_to_json_str);
+  /* JSON Object emission to string */
+  std::string file_outputs;
+  llvm::raw_string_ostream file_output_ss(file_outputs);
+  llvm::json::OStream json_os(file_output_ss);
   json_os.object([&]{
-    json_os.attribute("source", llvm::json::Value(optimized_source));
+    for (auto attr_pair : ip_json) {
+      json_os.attribute(attr_pair.first, attr_pair.second);
+    }
   });
 
   /* Final emission to JSON file */
   std::fstream fio;
   fio.open(filename, std::ios::trunc | std::ios::out | std::ios::in);
-  fio << json_file_contents;
+  fio << file_outputs;
 
-  return failure(state.encounteredError);
+  return LogicalResult::success();
 }
 
 void scalehls::registerEmitIPLibraryTranslation() {
   static TranslateFromMLIRRegistration toIPLibrary(
-      "emit-ip", emitIPLibrary, [&](DialectRegistry &registry) {
+    "emit-ip", emitIPLibrary, [&](DialectRegistry &registry) {
         scalehls::registerAllDialects(registry);
-      });
+      }
+  );
 }
