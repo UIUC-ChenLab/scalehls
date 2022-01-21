d65454f24e5ac4e6450aaed11150280e9d879b24
Hanchen Ye <hanchenye@gmail.com>
2022-01-21 03:26:51 -0600

    Clean up Conversion/Passes.h

--- include/scalehls/Conversion/Passes.h
+++ include/scalehls/Conversion/Passes.h
@@ -17,12 +17,6 @@ class Pass;
 namespace mlir {
 namespace scalehls {
 
-// SCF2Affine Raising passes
-std::unique_ptr<Pass> createRaiseLoadStorePass();
-std::unique_ptr<Pass> createRaiseSCFForPass();
-void LoadStoreRaisingPatterns(RewritePatternSet &patterns);
-void SCFForRaisingPatterns(RewritePatternSet &patterns);
-
 // HLSKernel and HLSCpp conversion passes.
 std::unique_ptr<Pass> createLegalizeToHLSCppPass();
 std::unique_ptr<Pass> createHLSKernelToAffinePass();
