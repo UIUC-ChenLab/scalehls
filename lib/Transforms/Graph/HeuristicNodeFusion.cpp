b722e77c7991cf577581ba5c4db56ebc64e3fc1f
Hanchen Ye <hanchenye@gmail.com>
2022-03-25 20:45:03 -0500

    [HeuristicNodeFusion] Refactor with stream node and output ops

--- lib/Transforms/Graph/HeuristicNodeFusion.cpp
+++ lib/Transforms/Graph/HeuristicNodeFusion.cpp
@@ -5,189 +5,174 @@
 //===----------------------------------------------------------------------===//
 
 #include "mlir/Dialect/Tosa/IR/TosaOps.h"
-#include "mlir/IR/Matchers.h"
-#include "mlir/IR/SymbolTable.h"
+#include "mlir/IR/Dominance.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "scalehls/Dialect/HLSCpp/HLSCpp.h"
 #include "scalehls/Transforms/Passes.h"
 #include "scalehls/Transforms/Utils.h"
 
 using namespace mlir;
 using namespace scalehls;
+using namespace hlscpp;
 
-/// Outline each op with the specified types into a new sub-function.
-template <typename... OpTypes> static void applyOutlining(FuncOp func) {
-  auto builder = OpBuilder(func);
-  localizeConstants(func.front());
+namespace {
+/// This pattern will outline ops with the specified type.
+template <typename OpType>
+struct OutliningPattern : public OpRewritePattern<OpType> {
+  using OpRewritePattern<OpType>::OpRewritePattern;
 
-  unsigned nodeIdx = 0;
-  for (auto &op : llvm::make_early_inc_range(func.getOps())) {
-    if (!isa<OpTypes...>(op))
-      continue;
+  LogicalResult matchAndRewrite(OpType op,
+                                PatternRewriter &rewriter) const override {
+    if (op->template getParentOfType<StreamNodeOp>())
+      return success();
 
-    SmallVector<tosa::ConstOp, 4> localConsts;
-    SmallVector<Value, 4> args;
-    SmallVector<Type, 4> argTypes;
-    for (auto operand : op.getOperands()) {
-      if (auto constOp = operand.template getDefiningOp<tosa::ConstOp>()) {
-        assert(constOp->hasOneUse() && "constant is not localized");
-        localConsts.push_back(constOp);
-        continue;
-      }
-      args.push_back(operand);
-      argTypes.push_back(operand.getType());
+    // Create a new stream node.
+    rewriter.setInsertionPoint(op);
+    auto node = rewriter.template create<StreamNodeOp>(op->getLoc(),
+                                                       op->getResultTypes());
+    op.replaceAllUsesWith(node);
+
+    // Create the output op of the node.
+    auto nodeBlock = rewriter.createBlock(&node.body());
+    rewriter.setInsertionPointToEnd(nodeBlock);
+    auto output =
+        rewriter.create<StreamOutputOp>(op->getLoc(), op->getResults());
+
+    // Move the target operation and constants into the node.
+    for (auto operand : op->getOperands())
+      if (auto constOp = operand.template getDefiningOp<tosa::ConstOp>())
+        constOp->moveBefore(output);
+    op->moveBefore(output);
+
+    return success();
+  }
+};
+} // namespace
+
+/// Fuse the given operation into the given stream node and return the fused
+/// stream node.
+static StreamNodeOp fuseOpIntoNode(Operation *op, StreamNodeOp node,
+                                   DominanceInfo &DT,
+                                   PatternRewriter &rewriter) {
+  assert(op != node && op->getBlock() == node->getBlock() &&
+         "must be different ops in the same block");
+
+  // Move the target op into the node.
+  auto nodeBlock = &node.body().front();
+  if (DT.dominates(op, node))
+    op->moveBefore(&nodeBlock->front());
+  else
+    op->moveBefore(&nodeBlock->back());
+
+  auto output = nodeBlock->getTerminator();
+  SmallVector<Value, 4> outputValues;
+  SmallVector<Value, 4> resultsToReplace;
+
+  // Collect output results from the target op.
+  for (auto result : op->getResults())
+    if (result.isUsedOutsideOfBlock(nodeBlock)) {
+      outputValues.push_back(result);
+      resultsToReplace.push_back(result);
     }
 
-    // Create new sub-function.
-    builder.setInsertionPoint(func);
-    auto subFuncName =
-        func.getName().str() + "_node" + std::to_string(nodeIdx++);
-    auto subFunc = builder.template create<FuncOp>(
-        op.getLoc(), subFuncName,
-        builder.getFunctionType(argTypes, op.getResult(0).getType()));
+  // Collect output results from the original node.
+  for (auto t : llvm::zip(output->getOperands(), node->getResults())) {
+    auto value = std::get<0>(t);
+    auto result = std::get<1>(t);
 
-    // Create sub-function call.
-    builder.setInsertionPoint(&op);
-    auto call =
-        builder.template create<func::CallOp>(op.getLoc(), subFunc, args);
-    op.replaceAllUsesWith(call);
+    // Update the operand of the target op.
+    for (auto &use : llvm::make_early_inc_range(result.getUses()))
+      if (use.getOwner() == op)
+        use.set(value);
 
-    // Create a return op for the sub-function.
-    auto entry = subFunc.addEntryBlock();
-    builder.setInsertionPointToEnd(entry);
-    auto returnOp =
-        builder.template create<func::ReturnOp>(op.getLoc(), op.getResult(0));
-
-    // Move local constants and conv2d/pool2d/matmul/add to the new created
-    // sub-function.
-    for (auto localConst : localConsts)
-      localConst->moveBefore(returnOp);
-    for (auto zip : llvm::zip(args, entry->getArguments()))
-      std::get<0>(zip).replaceUsesWithIf(std::get<1>(zip), [&](OpOperand &use) {
-        return use.getOwner() == &op;
-      });
-    op.moveBefore(returnOp);
+    if (result.isUsedOutsideOfBlock(nodeBlock)) {
+      outputValues.push_back(value);
+      resultsToReplace.push_back(result);
+    }
   }
+
+  SmallVector<Type, 4> outputTypes;
+  for (auto value : outputValues)
+    outputTypes.push_back(value.getType());
+
+  // Create new stream node and replace all uses.
+  rewriter.setInsertionPoint(node);
+  auto newNode = rewriter.create<StreamNodeOp>(node.getLoc(), outputTypes);
+  for (auto t : llvm::zip(resultsToReplace, newNode.getResults()))
+    std::get<0>(t).replaceAllUsesWith(std::get<1>(t));
+  rewriter.inlineRegionBefore(node.body(), newNode.body(),
+                              newNode.body().end());
+  rewriter.eraseOp(node);
+
+  // Create new stream output.
+  rewriter.setInsertionPoint(output);
+  rewriter.replaceOpWithNewOp<StreamOutputOp>(output, outputValues);
+  return newNode;
 }
 
 namespace {
-/// This pattern tries to fuse each op with the specified types forward if it
-/// appears before a sub-function call.
-template <typename... OpTypes>
-struct ForwardFusingRewritePattern : public OpRewritePattern<func::CallOp> {
-  ForwardFusingRewritePattern(MLIRContext *context, ModuleOp &module)
-      : OpRewritePattern<func::CallOp>(context), module(module) {}
-  using OpRewritePattern<func::CallOp>::OpRewritePattern;
+/// This pattern will forward fuse ops with the specified type.
+template <typename OpType>
+struct ForwardFusingPattern : public OpRewritePattern<OpType> {
+  ForwardFusingPattern(MLIRContext *context, DominanceInfo &DT)
+      : OpRewritePattern<OpType>(context), DT(DT) {}
+  using OpRewritePattern<OpType>::OpRewritePattern;
 
-  LogicalResult matchAndRewrite(func::CallOp call,
+  LogicalResult matchAndRewrite(OpType op,
                                 PatternRewriter &rewriter) const override {
-    auto func = module.lookupSymbol<FuncOp>(call.getCallee());
-    auto firstOp = &func.front().front();
+    if (op->template getParentOfType<StreamNodeOp>())
+      return success();
 
-    // Used to collect the new input types of the sub-function.
-    SmallVector<Type, 4> inputTypes;
-    for (auto &input : call->getOpOperands()) {
-      auto inputValue = input.get();
-
-      // TODO: This could be more flexible. For example, support multiple users.
-      if (inputValue.hasOneUse() && inputValue.getDefiningOp() &&
-          isa<OpTypes...>(inputValue.getDefiningOp())) {
-        auto defOp = inputValue.getDefiningOp();
-        auto &defOperand = defOp->getOpOperand(0);
-
-        // TODO: Support to add new arguments to the sub-function.
-        if (llvm::any_of(defOp->getOperands(), [&](Value v) {
-              return v != defOperand.get() && !v.getDefiningOp<tosa::ConstOp>();
-            }))
-          continue;
-
-        // Move into the sub-function and replace all its uses.
-        for (auto operand : defOp->getOperands())
-          if (auto localConst = operand.getDefiningOp<tosa::ConstOp>())
-            localConst->moveBefore(firstOp);
-        defOp->moveBefore(firstOp);
-
-        auto arg = func.getArgument(input.getOperandNumber());
-        arg.replaceAllUsesWith(inputValue);
-        input.set(defOperand.get());
-
-        // Set argument type and collect input types.
-        arg.setType(defOperand.get().getType());
-        defOperand.set(arg);
-        inputTypes.push_back(defOperand.get().getType());
-      } else
-        inputTypes.push_back(inputValue.getType());
+    // We always select the dominating node as the target node to fuse.
+    StreamNodeOp targetNode;
+    for (auto user : op->getUsers()) {
+      auto node = user->template getParentOfType<StreamNodeOp>();
+      if (!targetNode || (targetNode && node && DT.dominates(node, targetNode)))
+        targetNode = node;
     }
 
-    // Finally, we need to update the sub-function type.
-    if (inputTypes != func.getArgumentTypes()) {
-      func.setType(rewriter.getFunctionType(inputTypes, func.getResultTypes()));
+    if (targetNode) {
+      fuseOpIntoNode(op, targetNode, DT, rewriter);
       return success();
-    } else
-      return failure();
+    }
+    return failure();
   }
 
 private:
-  ModuleOp &module;
+  DominanceInfo &DT;
 };
 } // namespace
 
 namespace {
-/// This pattern tries to fuse each op with the specified types backward if it
-/// appears after a sub-function call.
-template <typename... OpTypes>
-struct BackwardFusingRewritePattern : public OpRewritePattern<func::CallOp> {
-  BackwardFusingRewritePattern(MLIRContext *context, ModuleOp &module)
-      : OpRewritePattern<func::CallOp>(context), module(module) {}
-  using OpRewritePattern<func::CallOp>::OpRewritePattern;
+/// This pattern will backward fuse ops with the specified type.
+template <typename OpType>
+struct BackwardFusingPattern : public OpRewritePattern<OpType> {
+  BackwardFusingPattern(MLIRContext *context, DominanceInfo &DT)
+      : OpRewritePattern<OpType>(context), DT(DT) {}
+  using OpRewritePattern<OpType>::OpRewritePattern;
 
-  LogicalResult matchAndRewrite(func::CallOp call,
+  LogicalResult matchAndRewrite(OpType op,
                                 PatternRewriter &rewriter) const override {
-    auto func = module.lookupSymbol<FuncOp>(call.getCallee());
-    auto returnOp = func.back().getTerminator();
+    if (op->template getParentOfType<StreamNodeOp>())
+      return success();
 
-    // Used to collect the new output types of the sub-function.
-    SmallVector<Type, 4> outputTypes;
-    for (auto result : call.getResults()) {
-      // TODO: This could be more flexible. For example, support multiple users.
-      if (result.hasOneUse() && isa<OpTypes...>(*result.user_begin())) {
-        auto &use = *result.use_begin();
-        auto user = use.getOwner();
-        auto userResult = user->getResult(0);
-
-        // TODO: Support to add new arguments to the sub-function.
-        if (llvm::any_of(user->getOperands(), [&](Value v) {
-              return v != use.get() && !v.getDefiningOp<tosa::ConstOp>();
-            }))
-          continue;
-
-        // Move into the sub-function and replace all its uses.
-        for (auto operand : user->getOperands())
-          if (auto localConst = operand.getDefiningOp<tosa::ConstOp>())
-            localConst->moveBefore(returnOp);
-        user->moveBefore(returnOp);
-
-        userResult.replaceAllUsesWith(result);
-        use.set(returnOp->getOperand(result.getResultNumber()));
-        returnOp->setOperand(result.getResultNumber(), userResult);
-
-        // Update the call result type and collect output types.
-        result.setType(userResult.getType());
-        outputTypes.push_back(userResult.getType());
-      } else
-        outputTypes.push_back(result.getType());
+    // We always select the dominated node as the target node to fuse.
+    StreamNodeOp targetNode;
+    for (auto operand : op->getOperands()) {
+      auto node = operand.template getDefiningOp<StreamNodeOp>();
+      if (!targetNode || (targetNode && node && DT.dominates(targetNode, node)))
+        targetNode = node;
     }
 
-    // Finally, we need to update the sub-function type.
-    if (outputTypes != func.getResultTypes()) {
-      func.setType(
-          rewriter.getFunctionType(func.getArgumentTypes(), outputTypes));
+    if (targetNode) {
+      fuseOpIntoNode(op, targetNode, DT, rewriter);
       return success();
-    } else
-      return failure();
+    }
+    return failure();
   }
 
 private:
-  ModuleOp &module;
+  DominanceInfo &DT;
 };
 } // namespace
 
@@ -197,18 +182,18 @@ struct HeuristicNodeFusion
   void runOnOperation() override {
     auto module = getOperation();
     auto context = module.getContext();
+    localizeConstants(module.body().front());
+    auto DT = DominanceInfo(module);
 
-    // Outline the computation intensive operations.
-    for (auto func : llvm::make_early_inc_range(module.getOps<FuncOp>()))
-      applyOutlining<tosa::Conv2DOp, tosa::AvgPool2dOp, tosa::MaxPool2dOp,
-                     tosa::MatMulOp, tosa::AddOp>(func);
-
-    // Fuse other operations into sub-functions.
     mlir::RewritePatternSet patterns(context);
-    patterns.add<BackwardFusingRewritePattern<tosa::ClampOp>>(context, module);
-    patterns
-        .add<ForwardFusingRewritePattern<tosa::ReshapeOp, tosa::TransposeOp>>(
-            context, module);
+    patterns.add<OutliningPattern<tosa::Conv2DOp>>(context);
+    patterns.add<OutliningPattern<tosa::AvgPool2dOp>>(context);
+    patterns.add<OutliningPattern<tosa::MaxPool2dOp>>(context);
+    patterns.add<OutliningPattern<tosa::MatMulOp>>(context);
+    patterns.add<OutliningPattern<tosa::AddOp>>(context);
+    patterns.add<BackwardFusingPattern<tosa::ClampOp>>(context, DT);
+    patterns.add<ForwardFusingPattern<tosa::ReshapeOp>>(context, DT);
+    patterns.add<ForwardFusingPattern<tosa::TransposeOp>>(context, DT);
     (void)applyPatternsAndFoldGreedily(module, std::move(patterns));
   }
 };
