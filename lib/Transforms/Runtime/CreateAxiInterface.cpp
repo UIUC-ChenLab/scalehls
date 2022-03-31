55ef92863121bf44107b136dc846ffbdf4597670
Hanchen Ye <hanchenye@gmail.com>
2022-03-31 03:50:04 -0500

    [CreateAxiInterface] Support to demote buffer primitives to runtime main; Restructure this pass to make it more robust

--- lib/Transforms/Runtime/CreateAxiInterface.cpp
+++ lib/Transforms/Runtime/CreateAxiInterface.cpp
@@ -13,11 +13,27 @@ using namespace mlir;
 using namespace scalehls;
 using namespace hls;
 
-// A helper to get corresponding DRAM memref type from normal memref type.
-static MemRefType getDramType(MemRefType type) {
-  return MemRefType::get(type.getShape(), type.getElementType(),
-                         type.getLayout().getAffineMap(),
-                         (unsigned)MemoryKind::DRAM);
+/// A helper to get corresponding DRAM memref type from normal memref type.
+static Type getDramType(Type type) {
+  if (auto memrefType = type.dyn_cast<MemRefType>())
+    return MemRefType::get(memrefType.getShape(), memrefType.getElementType(),
+                           memrefType.getLayout().getAffineMap(),
+                           (unsigned)MemoryKind::DRAM);
+  return type;
+}
+
+/// A helper to update function type recursively.
+void updateFuncType(func::FuncOp func, OpBuilder &builder) {
+  func.setType(
+      builder.getFunctionType(func.front().getArgumentTypes(),
+                              func.back().getTerminator()->getOperandTypes()));
+  for (auto subCall : func.getOps<func::CallOp>()) {
+    auto subFunc = SymbolTable::lookupNearestSymbolFrom<func::FuncOp>(
+        subCall, subCall.getCalleeAttr());
+    for (auto t : llvm::zip(subFunc.getArguments(), subCall.getOperandTypes()))
+      std::get<0>(t).setType(std::get<1>(t));
+    updateFuncType(subFunc, builder);
+  }
 }
 
 namespace {
@@ -30,7 +46,7 @@ struct CreateAxiInterface
     // Get the top and runtime function of the module.
     auto func = getTopFunc(module);
     auto runtime = getRuntimeFunc(module);
-    if (!func || !runtime ||
+    if (!func || !runtime || func.getNumResults() || runtime.getNumResults() ||
         !llvm::hasSingleElement(runtime.getOps<func::CallOp>())) {
       emitError(module.getLoc(), "fail to find legal top/runtime function");
       return signalPassFailure();
@@ -43,88 +59,74 @@ struct CreateAxiInterface
       return signalPassFailure();
     };
 
-    // First, convert the type of the runtime function.
-    for (auto arg : runtime.getArguments())
-      if (auto type = arg.getType().dyn_cast<MemRefType>())
-        arg.setType(getDramType(type));
-    runtime.setType(builder.getFunctionType(runtime.front().getArgumentTypes(),
-                                            runtime.getResultTypes()));
-
-    // Then, convert each constant to DRAM type.
-    for (auto toMemref : runtime.getOps<bufferization::ToMemrefOp>())
-      toMemref.memref().setType(
-          getDramType(toMemref.getType().cast<MemRefType>()));
-
-    // Third, move each allocated memory in the top function to the runtime
-    // function, and convert it to DRAM type.
+    // Move each memory, including normal allocs and buffer primitive, allocated
+    // in the top function to the runtime function. As each AXI interface only
+    // has one read and one write channel, we need to avoid interface conflicts
+    // by analyzing the memroy access pattern of sub-functions.
     SmallVector<Value, 32> inputs(call.getOperands());
-    for (auto alloc :
-         llvm::make_early_inc_range(func.getOps<memref::AllocOp>())) {
-      alloc.memref().setType(getDramType(alloc.getType()));
-      alloc->moveBefore(call);
+    for (auto &op : llvm::make_early_inc_range(func.front())) {
+      if (!isa<memref::AllocOp, PrimBufferOp>(op))
+        continue;
+      auto memref = op.getResult(0);
+      auto type = memref.getType().cast<MemRefType>();
+      op.moveBefore(call);
 
-      // Create a new interface
-      inputs.push_back(alloc);
-      auto arg = func.front().addArgument(alloc.getType(), alloc.getLoc());
-      bool hasWriteChannel = true;
-      bool hasReadChannel = true;
+      // Add a new AXI interface to the top function.
+      inputs.push_back(memref);
+      auto interface = func.front().addArgument(type, op.getLoc());
+      bool writeChannel = true, readChannel = true;
 
-      for (auto &use : llvm::make_early_inc_range(alloc->getUses())) {
+      for (auto &use : llvm::make_early_inc_range(op.getUses())) {
         if (auto subCall = dyn_cast<func::CallOp>(use.getOwner())) {
-          auto subFunc = module.lookupSymbol<FuncOp>(subCall.getCallee());
-          auto memref = subFunc.getArgument(use.getOperandNumber());
+          auto arg = module.lookupSymbol<FuncOp>(subCall.getCallee())
+                         .getArgument(use.getOperandNumber());
 
-          auto readFlag = llvm::any_of(memref.getUsers(), [](Operation *op) {
-            return isa<mlir::AffineReadOpInterface, memref::LoadOp,
-                       func::CallOp>(op);
+          auto readFlag = llvm::any_of(arg.getUsers(), [](Operation *op) {
+            return isa<mlir::AffineReadOpInterface, func::CallOp>(op);
           });
-          auto writeFlag = llvm::any_of(memref.getUsers(), [](Operation *op) {
-            return isa<mlir::AffineWriteOpInterface, memref::StoreOp,
-                       func::CallOp>(op);
+          auto writeFlag = llvm::any_of(arg.getUsers(), [](Operation *op) {
+            return isa<mlir::AffineWriteOpInterface, func::CallOp>(op);
           });
 
-          // If the read/write is already occupied, create a new interface.
-          if ((readFlag && !hasReadChannel) ||
-              (writeFlag && !hasWriteChannel)) {
-            inputs.push_back(alloc);
-            arg = func.front().addArgument(alloc.getType(), alloc.getLoc());
-            hasReadChannel = true;
-            hasWriteChannel = true;
+          // If the read/write is already occupied, add a new AXI interface.
+          if ((readFlag && !readChannel) || (writeFlag && !writeChannel)) {
+            inputs.push_back(memref);
+            interface = func.front().addArgument(type, op.getLoc());
+            writeChannel = true, readChannel = true;
           }
 
-          // Occupy read/write channel if applicable.
+          // Occupy the current read/write channel.
           if (readFlag)
-            hasReadChannel = false;
+            readChannel = false;
           if (writeFlag)
-            hasWriteChannel = false;
-        } else
-          llvm_unreachable("memref must be used by call op");
+            writeChannel = false;
+        } else {
+          use.getOwner()->emitOpError("memref must be used by call op");
+          return signalPassFailure();
+        }
 
         // Set the current use to the current interface argument.
-        use.set(arg);
+        use.set(interface);
       }
     }
 
-    // Forth, update the top function call.
+    // Update the top function and call.
     builder.setInsertionPoint(call);
     auto newCall = builder.create<func::CallOp>(call.getLoc(), func.getName(),
                                                 func.getResultTypes(), inputs);
     call.replaceAllUsesWith(newCall);
     call.erase();
-
-    // Fifth, convert the type of top function.
-    for (auto zip : llvm::zip(func.getArguments(), newCall.getOperandTypes()))
-      std::get<0>(zip).setType(std::get<1>(zip));
     func.setType(newCall.getCalleeType());
 
-    // Finally, convert the type of each sub-function.
-    for (auto subCall : func.getOps<func::CallOp>()) {
-      auto subFunc = module.lookupSymbol<FuncOp>(subCall.getCallee());
-      for (auto zip :
-           llvm::zip(subFunc.getArguments(), subCall.getOperandTypes()))
-        std::get<0>(zip).setType(std::get<1>(zip));
-      subFunc.setType(subCall.getCalleeType());
-    }
+    // Convert each memory in the runtime function to DRAM type.
+    for (auto arg : runtime.getArguments())
+      arg.setType(getDramType(arg.getType()));
+    for (auto &op : runtime.front())
+      for (auto result : op.getResults())
+        result.setType(getDramType(result.getType()));
+
+    // Update function type recursively.
+    updateFuncType(runtime, builder);
   }
 };
 } // namespace
