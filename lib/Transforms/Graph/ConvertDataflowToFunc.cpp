9899afd7138b4779e6ac6809014c50c5648b1884
Hanchen Ye <hanchenye@gmail.com>
2022-03-31 03:49:01 -0500

    [HLS] Add a buffer primitive op; [ConvertDataflowToFunc] Support to convert dataflow buffer to primitive buffer

--- lib/Transforms/Graph/ConvertDataflowToFunc.cpp
+++ lib/Transforms/Graph/ConvertDataflowToFunc.cpp
@@ -5,6 +5,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "mlir/Analysis/Liveness.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Transforms/DialectConversion.h"
 #include "scalehls/Dialect/HLS/HLS.h"
 #include "scalehls/Transforms/Passes.h"
 #include "scalehls/Transforms/Utils.h"
@@ -14,21 +16,92 @@ using namespace scalehls;
 using namespace hls;
 
 namespace {
+struct NodeConversionPattern : public OpRewritePattern<DataflowNodeOp> {
+  NodeConversionPattern(MLIRContext *context, StringRef prefix,
+                        unsigned &nodeIdx)
+      : OpRewritePattern<DataflowNodeOp>(context), prefix(prefix),
+        nodeIdx(nodeIdx) {}
+
+  LogicalResult matchAndRewrite(DataflowNodeOp op,
+                                PatternRewriter &rewriter) const override {
+    // Analyze the input values and types of the function to create.
+    auto liveness = Liveness(op);
+    SmallVector<Value, 8> inputs;
+    SmallVector<Type, 8> inputTypes;
+    for (auto livein : liveness.getLiveIn(op.getBody())) {
+      if (op->isAncestor(livein.getParentBlock()->getParentOp()))
+        continue;
+      inputs.push_back(livein);
+      inputTypes.push_back(livein.getType());
+    }
+
+    // Create a new sub-function.
+    rewriter.setInsertionPoint(op->getParentOfType<func::FuncOp>());
+    auto subFunc = rewriter.create<func::FuncOp>(
+        op.getLoc(), prefix.str() + "_node" + std::to_string(nodeIdx++),
+        rewriter.getFunctionType(inputTypes, TypeRange()));
+
+    // Inline the contents of the dataflow node.
+    rewriter.inlineRegionBefore(op.getBodyRegion(), subFunc.getBody(),
+                                subFunc.end());
+    auto entry = &subFunc.front();
+    for (auto input : inputs) {
+      auto arg = entry->addArgument(input.getType(), input.getLoc());
+      input.replaceUsesWithIf(arg, [&](OpOperand &use) {
+        return subFunc->isAncestor(use.getOwner());
+      });
+    }
+
+    // Replace the original output terminator and dataflow node itself.
+    auto output = entry->getTerminator();
+    rewriter.setInsertionPoint(output);
+    rewriter.replaceOpWithNewOp<func::ReturnOp>(output);
+    rewriter.setInsertionPoint(op);
+    rewriter.create<func::CallOp>(op.getLoc(), subFunc, inputs);
+    rewriter.eraseOp(op);
+    return success();
+  }
+
+private:
+  StringRef prefix;
+  unsigned &nodeIdx;
+};
+} // namespace
+
+namespace {
+struct BufferConversionPattern : public OpRewritePattern<DataflowBufferOp> {
+  using OpRewritePattern<DataflowBufferOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(DataflowBufferOp op,
+                                PatternRewriter &rewriter) const override {
+    if (auto alloc = op.input().getDefiningOp<memref::AllocOp>()) {
+      rewriter.setInsertionPoint(alloc);
+      auto primBuffer = rewriter.replaceOpWithNewOp<PrimBufferOp>(
+          op, op.getType(), op.depth());
+      rewriter.replaceOp(alloc, primBuffer.memref());
+      return success();
+    }
+    return failure();
+  }
+};
+} // namespace
+
+namespace {
 struct ConvertDataflowToFunc
     : public ConvertDataflowToFuncBase<ConvertDataflowToFunc> {
   void runOnOperation() override {
     auto module = getOperation();
-    auto builder = OpBuilder(module);
-    auto loc = builder.getUnknownLoc();
+    auto context = module.getContext();
     localizeConstants(*module.getBody());
 
     for (auto func :
          llvm::make_early_inc_range(module.getOps<func::FuncOp>())) {
       // Hoist the memrefs and stream channels outputed by each node.
-      for (auto node :
-           llvm::make_early_inc_range(func.getOps<DataflowNodeOp>())) {
+      // TODO: Maybe this should be factored out to somewhere else.
+      for (auto node : func.getOps<DataflowNodeOp>()) {
         auto output = node.getOutputOp();
         for (auto &use : output->getOpOperands()) {
+          // TODO: Support other types of outputs?
           if (!use.get().getType().isa<MemRefType, StreamType>()) {
             output.emitOpError("dataflow output has not been bufferized");
             return signalPassFailure();
@@ -41,46 +114,19 @@ struct ConvertDataflowToFunc
         }
       }
 
-      auto liveness = Liveness(func);
+      // Convert dataflow node and buffer operations.
+      ConversionTarget target(*context);
+      target.addIllegalOp<DataflowNodeOp, DataflowOutputOp, DataflowBufferOp>();
+      target.addLegalOp<func::FuncOp, func::ReturnOp, func::CallOp,
+                        PrimBufferOp>();
+
       unsigned nodeIdx = 0;
-      for (auto node :
-           llvm::make_early_inc_range(func.getOps<DataflowNodeOp>())) {
-        // Analyze the input values and types of the function to create.
-        SmallVector<Value, 8> inputs;
-        SmallVector<Type, 8> inputTypes;
-        for (auto livein : liveness.getLiveIn(node.getBody())) {
-          if (node->isAncestor(livein.getParentBlock()->getParentOp()))
-            continue;
-          inputs.push_back(livein);
-          inputTypes.push_back(livein.getType());
-        }
+      mlir::RewritePatternSet patterns(context);
+      patterns.add<NodeConversionPattern>(context, func.getName(), nodeIdx);
+      patterns.add<BufferConversionPattern>(context);
 
-        // Create a new sub-function.
-        builder.setInsertionPoint(func);
-        auto subFunc = builder.create<func::FuncOp>(
-            loc, func.getName().str() + "_node" + std::to_string(nodeIdx++),
-            builder.getFunctionType(inputTypes, TypeRange()));
-
-        // Create entry block and return operation.
-        auto entry = subFunc.addEntryBlock();
-        for (auto t : llvm::zip(inputs, entry->getArguments()))
-          std::get<0>(t).replaceUsesWithIf(std::get<1>(t), [&](OpOperand &use) {
-            return node->isAncestor(use.getOwner());
-          });
-        builder.setInsertionPointToEnd(entry);
-        builder.create<func::ReturnOp>(loc);
-
-        // Inline the contents of the dataflow node.
-        auto &subFuncOps = entry->getOperations();
-        auto &nodeOps = node.getBody()->getOperations();
-        subFuncOps.splice(subFuncOps.begin(), nodeOps, nodeOps.begin(),
-                          std::prev(nodeOps.end()));
-
-        // Create a function call to replace the original node.
-        builder.setInsertionPoint(node);
-        builder.create<func::CallOp>(node.getLoc(), subFunc, inputs);
-        node.erase();
-      }
+      if (failed(applyPartialConversion(func, target, std::move(patterns))))
+        return signalPassFailure();
     }
   }
 };
