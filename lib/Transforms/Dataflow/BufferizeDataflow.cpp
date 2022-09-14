2082fd49e694ebf715677aee0428d8c629b52a8c
Hanchen Ye <hanchenye@gmail.com>
2022-09-13 22:42:59 -0500

    Demote getGlobalOps in BuferizeDataflow out of taskOps

--- lib/Transforms/Dataflow/BufferizeDataflow.cpp
+++ lib/Transforms/Dataflow/BufferizeDataflow.cpp
@@ -102,6 +102,45 @@ struct TaskBufferizationPattern : public OpRewritePattern<TaskOp> {
 } // namespace
 
 namespace {
+struct GetGlobalDemotePattern : public OpRewritePattern<TaskOp> {
+  using OpRewritePattern<TaskOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(TaskOp task,
+                                PatternRewriter &rewriter) const override {
+    bool hasChanged = false;
+
+    SmallVector<Value, 4> memrefs;
+    SmallVector<Location, 4> locs;
+    for (auto getGlobal :
+         llvm::make_early_inc_range(task.getOps<memref::GetGlobalOp>()))
+      if (getGlobal.getType().getNumElements() > 1024) {
+        getGlobal->moveBefore(task);
+        memrefs.push_back(getGlobal);
+        locs.push_back(getGlobal.getLoc());
+        hasChanged = true;
+      }
+
+    if (hasChanged) {
+      auto args = task.getBody()->addArguments(ValueRange(memrefs), locs);
+      for (auto t : llvm::zip(memrefs, args))
+        std::get<0>(t).replaceAllUsesWith(std::get<1>(t));
+
+      SmallVector<Value, 16> newInputs(task.inputs());
+      newInputs.append(memrefs.begin(), memrefs.end());
+      rewriter.setInsertionPoint(task);
+      auto newTask = rewriter.create<TaskOp>(task.getLoc(),
+                                             task.getResultTypes(), newInputs);
+      rewriter.inlineRegionBefore(task.body(), newTask.body(),
+                                  newTask.body().end());
+
+      rewriter.replaceOp(task, newTask.getResults());
+    }
+    return success(hasChanged);
+  }
+};
+} // namespace
+
+namespace {
 struct BufferizeDataflow : public BufferizeDataflowBase<BufferizeDataflow> {
   void runOnOperation() override {
     auto func = getOperation();
@@ -110,6 +149,11 @@ struct BufferizeDataflow : public BufferizeDataflowBase<BufferizeDataflow> {
     mlir::RewritePatternSet patterns(context);
     patterns.add<TaskBufferizationPattern>(context);
     patterns.add<ScheduleBufferizationPattern>(context);
+    (void)applyPatternsAndFoldGreedily(func, std::move(patterns));
+
+    // TODO: Temporary approach. This should be factored out.
+    patterns.clear();
+    patterns.add<GetGlobalDemotePattern>(context);
     (void)applyPatternsAndFoldGreedily(func, std::move(patterns));
   }
 };
