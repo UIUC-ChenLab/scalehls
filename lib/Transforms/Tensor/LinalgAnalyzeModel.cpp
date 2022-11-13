64c7993f479d1cf17a3280bf83edcfe7d5b6fe4d
Hanchen Ye <hanchenye@gmail.com>
2022-11-12 23:32:09 -0600

    Support analyze depthwise conv layers

--- lib/Transforms/Tensor/LinalgAnalyzeModel.cpp
+++ lib/Transforms/Tensor/LinalgAnalyzeModel.cpp
@@ -25,43 +25,72 @@ struct LinalgAnalyzeModel : public LinalgAnalyzeModelBase<LinalgAnalyzeModel> {
     unsigned layerIdx = 0;
     unsigned long numOps = 0;
 
-    for (auto convOp : func.getOps<linalg::Conv2DNchwFchwOp>()) {
-      auto weightShape =
-          convOp.filter().getType().cast<RankedTensorType>().getShape();
-      auto outputShape =
-          convOp->getResult(0).getType().cast<RankedTensorType>().getShape();
+    for (auto &op : func.getOps()) {
+      if (auto convOp = dyn_cast<linalg::Conv2DNchwFchwOp>(op)) {
+        auto weightShape =
+            convOp.filter().getType().cast<RankedTensorType>().getShape();
+        auto outputShape =
+            convOp->getResult(0).getType().cast<RankedTensorType>().getShape();
 
-      auto batch = outputShape[0];
-      auto height = outputShape[2];
-      auto width = outputShape[3];
-      auto in_filter = weightShape[0];
-      auto out_filter = weightShape[1];
-      auto hkernel = weightShape[2];
-      auto wkernel = weightShape[3];
+        auto batch = outputShape[0];
+        auto height = outputShape[2];
+        auto width = outputShape[3];
+        auto in_filter = weightShape[0];
+        auto out_filter = weightShape[1];
+        auto hkernel = weightShape[2];
+        auto wkernel = weightShape[3];
 
-      numOps += (2 * batch * height * width * in_filter * out_filter * hkernel *
-                 wkernel);
-      llvm::dbgs() << "(\"conv" << layerIdx << "\", Workload(" << batch << ", "
-                   << height << ", " << width << ", " << in_filter << ", "
-                   << out_filter << ", " << hkernel << ", " << wkernel
-                   << ")),\n";
-      ++layerIdx;
-    }
+        auto ops = 2 * batch * height * width * in_filter * out_filter *
+                   hkernel * wkernel;
+        numOps += ops;
 
-    for (auto gemmOp : func.getOps<linalg::MatmulOp>()) {
-      auto inputShape =
-          gemmOp.getOperand(0).getType().cast<RankedTensorType>().getShape();
-      auto weightShape =
-          gemmOp.getOperand(1).getType().cast<RankedTensorType>().getShape();
+        llvm::dbgs() << "(\"conv" << layerIdx << "\", Workload(" << batch
+                     << ", " << height << ", " << width << ", " << in_filter
+                     << ", " << out_filter << ", " << hkernel << ", " << wkernel
+                     << "), Complexity(" << ops << ")),\n";
+        ++layerIdx;
 
-      auto batch = inputShape[0];
-      auto in_filter = weightShape[0];
-      auto out_filter = weightShape[1];
+      } else if (auto depthOp =
+                     dyn_cast<linalg::DepthwiseConv2DNchwChwOp>(op)) {
+        auto weightShape =
+            depthOp.filter().getType().cast<RankedTensorType>().getShape();
+        auto outputShape =
+            depthOp->getResult(0).getType().cast<RankedTensorType>().getShape();
 
-      numOps += (2 * batch * in_filter * out_filter);
-      llvm::dbgs() << "(\"matmul" << layerIdx << "\", Workload(" << batch
-                   << ", " << in_filter << ", " << out_filter << ")),\n";
-      ++layerIdx;
+        auto batch = outputShape[0];
+        auto height = outputShape[2];
+        auto width = outputShape[3];
+        auto filter = weightShape[0];
+        auto hkernel = weightShape[1];
+        auto wkernel = weightShape[2];
+
+        auto ops = 2 * batch * height * width * filter * hkernel * wkernel;
+        numOps += ops;
+
+        llvm::dbgs() << "(\"depth_conv" << layerIdx << "\", Workload(" << batch
+                     << ", " << height << ", " << width << ", " << filter
+                     << ", " << hkernel << ", " << wkernel << "), Complexity("
+                     << ops << ")),\n";
+        ++layerIdx;
+
+      } else if (auto gemmOp = dyn_cast<linalg::MatmulOp>(op)) {
+        auto inputShape =
+            gemmOp.getOperand(0).getType().cast<RankedTensorType>().getShape();
+        auto weightShape =
+            gemmOp.getOperand(1).getType().cast<RankedTensorType>().getShape();
+
+        auto batch = inputShape[0];
+        auto in_filter = weightShape[0];
+        auto out_filter = weightShape[1];
+
+        auto ops = 2 * batch * in_filter * out_filter;
+        numOps += ops;
+
+        llvm::dbgs() << "(\"matmul" << layerIdx << "\", Workload(" << batch
+                     << ", " << in_filter << ", " << out_filter
+                     << "), Complexity(" << ops << ")),\n";
+        ++layerIdx;
+      }
     }
 
     llvm::dbgs() << "TOTAL OPS NUM: " << numOps << "\n";
