d5e2336ba36d02d46e59c736008a5c046f098e29
Hanchen Ye <hanchenye@gmail.com>
2023-04-09 18:45:21 -0500

    Don't partition external buffers in array partition

--- lib/Transforms/Directive/ArrayPartition.cpp
+++ lib/Transforms/Directive/ArrayPartition.cpp
@@ -49,7 +49,7 @@ bool scalehls::applyArrayPartition(Value array, ArrayRef<unsigned> factors,
                                    ArrayRef<hls::PartitionKind> kinds,
                                    bool updateFuncSignature) {
   auto arrayType = array.getType().dyn_cast<MemRefType>();
-  if (!arrayType || !arrayType.hasStaticShape() ||
+  if (!arrayType || isExtBuffer(array) || !arrayType.hasStaticShape() ||
       (int64_t)factors.size() != arrayType.getRank() ||
       (int64_t)kinds.size() != arrayType.getRank())
     return false;
