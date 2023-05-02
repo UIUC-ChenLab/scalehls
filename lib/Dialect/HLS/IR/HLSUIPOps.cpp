948fb01c7ac35492a2c1c846fabcd0e303a2d7e3
Hanchen Ye <hanchenye@gmail.com>
2023-05-02 16:00:55 -0500

    [HLS] Add UIP operations

--- lib/Dialect/HLS/IR/HLSUIPOps.cpp
+++ lib/Dialect/HLS/IR/HLSUIPOps.cpp
@@ -9,3 +9,15 @@
 using namespace mlir;
 using namespace scalehls;
 using namespace hls;
+
+//===----------------------------------------------------------------------===//
+// InstanceOp
+//===----------------------------------------------------------------------===//
+
+LogicalResult InstanceOp::verifySymbolUses(mlir::SymbolTableCollection &table) {
+  auto param = table.lookupNearestSymbolFrom<DeclareOp>(
+      (*this)->getParentOfType<ModuleOp>(), getIpAttr());
+  if (!param)
+    return (*this)->emitOpError("unknown IP name ") << getIpAttr();
+  return success(param);
+}
