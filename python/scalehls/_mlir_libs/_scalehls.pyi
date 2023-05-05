e7bb6585ae1913378fe9153bf398cfe46b3b4e0f
Hanchen Ye <hanchenye@gmail.com>
2023-05-04 20:26:11 -0500

    [Python] Add .pyi file for the scalehls extension

---
+++ python/scalehls/_mlir_libs/_scalehls.pyi
@@ -0,0 +1,15 @@
+"""ScaleHLS Python Native Extension"""
+from __future__ import annotations
+import _scalehls
+import typing
+
+__all__ = [
+    "emit_hlscpp",
+    "register_dialects"
+]
+
+
+def emit_hlscpp(arg0: MlirModule, arg1: object) -> bool:
+    pass
+def register_dialects(arg0: object) -> None:
+    pass
