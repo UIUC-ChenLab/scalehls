ab2f814f55fb899fb467342fd6667979d2d4326d
Hanchen Ye <hanchenye@gmail.com>
2024-02-22 17:09:57 -0600

    Update functional dataflow ops definition

--- lib/Dialect/HLS/IR/HLSOps.cpp
+++ lib/Dialect/HLS/IR/HLSOps.cpp
@@ -14,178 +14,6 @@ using namespace hls;
 using namespace affine;
 
 //===----------------------------------------------------------------------===//
-// DispatchOp
-//===----------------------------------------------------------------------===//
-
-namespace {
-template <typename OpType>
-struct SimplifyDispatchOrTaskOutputs : public OpRewritePattern<OpType> {
-  using OpRewritePattern<OpType>::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(OpType op,
-                                PatternRewriter &rewriter) const override {
-    auto yield = op.getYieldOp();
-    bool hasUnusedPort = false;
-
-    // Identify output values that are used.
-    SmallVector<Value, 4> usedOutputs;
-    SmallVector<Value, 4> usedResults;
-    for (auto result : op.getResults())
-      if (result.use_empty()) {
-        hasUnusedPort = true;
-      } else {
-        usedOutputs.push_back(yield.getOperand(result.getResultNumber()));
-        usedResults.push_back(result);
-      }
-
-    // Construct new op with only used outputs.
-    if (hasUnusedPort) {
-      rewriter.setInsertionPoint(yield);
-      rewriter.replaceOpWithNewOp<YieldOp>(yield, usedOutputs);
-
-      rewriter.setInsertionPoint(op);
-      auto newTask =
-          rewriter.create<OpType>(op.getLoc(), ValueRange(usedOutputs));
-      rewriter.inlineRegionBefore(op.getBody(), newTask.getBody(),
-                                  newTask.getBody().end());
-      for (auto t : llvm::zip(usedResults, newTask.getResults()))
-        std::get<0>(t).replaceAllUsesWith(std::get<1>(t));
-
-      rewriter.eraseOp(op);
-      return success();
-    }
-    return failure();
-  }
-};
-} // namespace
-
-namespace {
-template <typename OpType>
-struct InlineDispatchOrTask : public OpRewritePattern<OpType> {
-  InlineDispatchOrTask(MLIRContext *context,
-                       llvm::function_ref<bool(OpType)> condition)
-      : OpRewritePattern<OpType>(context), condition(condition) {}
-
-  LogicalResult matchAndRewrite(OpType op,
-                                PatternRewriter &rewriter) const override {
-    if (condition(op)) {
-      auto &ops = op.getBody().front().getOperations();
-      auto &parentOps = op->getBlock()->getOperations();
-      parentOps.splice(op->getIterator(), ops, ops.begin(),
-                       std::prev(ops.end()));
-      rewriter.replaceOp(op, op.getYieldOp()->getOperands());
-      return success();
-    }
-    return failure();
-  }
-
-private:
-  llvm::function_ref<bool(OpType)> condition;
-};
-} // namespace
-
-namespace {
-template <typename OpType>
-struct DemoteYieldedBuffer : public OpRewritePattern<OpType> {
-  using OpRewritePattern<OpType>::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(OpType op,
-                                PatternRewriter &rewriter) const override {
-    auto yield = op.getYieldOp();
-    bool hasChanged = false;
-
-    // Eliminat each yielded buffer. It's always safe to move the buffer to
-    // higher level hierarchy.
-    for (auto [yieldedValue, result] :
-         llvm::zip(yield.getOperands(), op.getResults()))
-      if (auto buffer = yieldedValue.template getDefiningOp<BufferOp>()) {
-        if (op->isAncestor(buffer))
-          buffer->moveBefore(op);
-
-        rewriter.replaceAllUsesWith(result, buffer);
-        hasChanged = true;
-      }
-    return success(hasChanged);
-  }
-};
-} // namespace
-
-void DispatchOp::getCanonicalizationPatterns(RewritePatternSet &results,
-                                             MLIRContext *context) {
-  results.add<SimplifyDispatchOrTaskOutputs<DispatchOp>>(context);
-  results.add<InlineDispatchOrTask<DispatchOp>>(context, [](DispatchOp op) {
-    return op.getOps<TaskOp>().empty() || llvm::hasSingleElement(op.getOps());
-  });
-  results.add<DemoteYieldedBuffer<DispatchOp>>(context);
-}
-
-LogicalResult DispatchOp::verify() {
-  if (getResultTypes() != getYieldOp().getOperandTypes())
-    return emitOpError("yield type doesn't align with result type");
-  return success();
-}
-
-/// Get the terminator yield op.
-YieldOp DispatchOp::getYieldOp() {
-  return cast<YieldOp>(getBody().front().getTerminator());
-}
-
-//===----------------------------------------------------------------------===//
-// TaskOp
-//===----------------------------------------------------------------------===//
-
-void TaskOp::getCanonicalizationPatterns(RewritePatternSet &results,
-                                         MLIRContext *context) {
-  results.add<SimplifyDispatchOrTaskOutputs<TaskOp>>(context);
-  results.add<InlineDispatchOrTask<TaskOp>>(
-      context, [](TaskOp op) { return llvm::hasSingleElement(op.getOps()); });
-  results.add<DemoteYieldedBuffer<TaskOp>>(context);
-}
-
-LogicalResult TaskOp::verify() {
-  if (getResultTypes() != getYieldOp().getOperandTypes())
-    return emitOpError("yield type doesn't align with result type");
-  return success();
-}
-
-/// Get the parent dispatch op.
-DispatchOp TaskOp::getDispatchOp() {
-  return (*this)->getParentOfType<DispatchOp>();
-}
-
-/// Get the terminator yield op.
-YieldOp TaskOp::getYieldOp() {
-  return cast<YieldOp>(getBody().front().getTerminator());
-}
-
-/// Get the immediate included linalg op. Will return nullptr if there is no
-/// such linalg op or more than one linalg op.
-linalg::LinalgOp TaskOp::getPayloadLinalgOp() {
-  auto linalgOps = getOps<linalg::LinalgOp>();
-  if (llvm::hasSingleElement(linalgOps))
-    return *linalgOps.begin();
-  return nullptr;
-}
-
-bool TaskOp::isLivein(Value value) {
-  auto liveins = Liveness(*this).getLiveIn(&(*this).getBody().front());
-  return liveins.count(value);
-}
-
-SmallVector<Value> TaskOp::getLiveins() {
-  auto liveins = Liveness(*this).getLiveIn(&(*this).getBody().front());
-  return {liveins.begin(), liveins.end()};
-}
-
-SmallVector<Operation *> TaskOp::getLiveinUsers(Value livein) {
-  assert(isLivein(livein) && "invalid livein");
-  auto users = llvm::make_filter_range(livein.getUsers(), [&](Operation *user) {
-    return (*this)->isAncestor(user);
-  });
-  return {users.begin(), users.end()};
-}
-
-//===----------------------------------------------------------------------===//
 // TensorInitOp
 //===----------------------------------------------------------------------===//
 
@@ -434,6 +262,172 @@ LogicalResult StreamCastOp::verify() {
 
 OpFoldResult StreamCastOp::fold(FoldAdaptor adaptor) {
   return foldStreamViewLikeInterface(*this);
+}
+
+//===----------------------------------------------------------------------===//
+// DispatchOp
+//===----------------------------------------------------------------------===//
+
+namespace {
+template <typename OpType>
+struct SimplifyDispatchOrTaskOutputs : public OpRewritePattern<OpType> {
+  using OpRewritePattern<OpType>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(OpType op,
+                                PatternRewriter &rewriter) const override {
+    auto yield = op.getYieldOp();
+    bool hasUnusedPort = false;
+
+    // Identify output values that are used.
+    SmallVector<Value, 4> usedOutputs;
+    SmallVector<Value, 4> usedResults;
+    for (auto result : op.getResults())
+      if (result.use_empty()) {
+        hasUnusedPort = true;
+      } else {
+        usedOutputs.push_back(yield.getOperand(result.getResultNumber()));
+        usedResults.push_back(result);
+      }
+
+    // Construct new op with only used outputs.
+    if (hasUnusedPort) {
+      rewriter.setInsertionPoint(yield);
+      rewriter.replaceOpWithNewOp<YieldOp>(yield, usedOutputs);
+
+      rewriter.setInsertionPoint(op);
+      auto newTask =
+          rewriter.create<OpType>(op.getLoc(), ValueRange(usedOutputs));
+      rewriter.inlineRegionBefore(op.getBody(), newTask.getBody(),
+                                  newTask.getBody().end());
+      for (auto t : llvm::zip(usedResults, newTask.getResults()))
+        std::get<0>(t).replaceAllUsesWith(std::get<1>(t));
+
+      rewriter.eraseOp(op);
+      return success();
+    }
+    return failure();
+  }
+};
+} // namespace
+
+namespace {
+template <typename OpType>
+struct InlineDispatchOrTask : public OpRewritePattern<OpType> {
+  InlineDispatchOrTask(MLIRContext *context,
+                       llvm::function_ref<bool(OpType)> condition)
+      : OpRewritePattern<OpType>(context), condition(condition) {}
+
+  LogicalResult matchAndRewrite(OpType op,
+                                PatternRewriter &rewriter) const override {
+    if (condition(op)) {
+      auto &ops = op.getBody().front().getOperations();
+      auto &parentOps = op->getBlock()->getOperations();
+      parentOps.splice(op->getIterator(), ops, ops.begin(),
+                       std::prev(ops.end()));
+      rewriter.replaceOp(op, op.getYieldOp()->getOperands());
+      return success();
+    }
+    return failure();
+  }
+
+private:
+  llvm::function_ref<bool(OpType)> condition;
+};
+} // namespace
+
+namespace {
+template <typename OpType>
+struct DemoteYieldedBuffer : public OpRewritePattern<OpType> {
+  using OpRewritePattern<OpType>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(OpType op,
+                                PatternRewriter &rewriter) const override {
+    auto yield = op.getYieldOp();
+    bool hasChanged = false;
+
+    // Eliminat each yielded buffer. It's always safe to move the buffer to
+    // higher level hierarchy.
+    for (auto [yieldedValue, result] :
+         llvm::zip(yield.getOperands(), op.getResults()))
+      if (auto buffer = yieldedValue.template getDefiningOp<BufferOp>()) {
+        if (op->isAncestor(buffer))
+          buffer->moveBefore(op);
+
+        rewriter.replaceAllUsesWith(result, buffer);
+        hasChanged = true;
+      }
+    return success(hasChanged);
+  }
+};
+} // namespace
+
+void DispatchOp::getCanonicalizationPatterns(RewritePatternSet &results,
+                                             MLIRContext *context) {
+  results.add<SimplifyDispatchOrTaskOutputs<DispatchOp>>(context);
+  results.add<InlineDispatchOrTask<DispatchOp>>(context, [](DispatchOp op) {
+    return op.getOps<TaskOp>().empty() || llvm::hasSingleElement(op.getOps());
+  });
+  results.add<DemoteYieldedBuffer<DispatchOp>>(context);
+}
+
+LogicalResult DispatchOp::verify() {
+  if (getResultTypes() != getYieldOp().getOperandTypes())
+    return emitOpError("yield type doesn't align with result type");
+  return success();
+}
+
+/// Get the terminator yield op.
+YieldOp DispatchOp::getYieldOp() {
+  return cast<YieldOp>(getBody().front().getTerminator());
+}
+
+//===----------------------------------------------------------------------===//
+// TaskOp
+//===----------------------------------------------------------------------===//
+
+void TaskOp::getCanonicalizationPatterns(RewritePatternSet &results,
+                                         MLIRContext *context) {
+  results.add<SimplifyDispatchOrTaskOutputs<TaskOp>>(context);
+  results.add<InlineDispatchOrTask<TaskOp>>(context, [](TaskOp op) {
+    return llvm::hasSingleElement(
+               op.getParentOp<DispatchOp>().getOps<TaskOp>()) ||
+           llvm::hasSingleElement(op.getOps());
+  });
+  results.add<DemoteYieldedBuffer<TaskOp>>(context);
+}
+
+LogicalResult TaskOp::verify() {
+  if (getResultTypes() != getYieldOp().getOperandTypes())
+    return emitOpError("yield type doesn't align with result type");
+  return success();
+}
+
+/// Get the parent dispatch op.
+DispatchOp TaskOp::getDispatchOp() {
+  return (*this)->getParentOfType<DispatchOp>();
+}
+
+/// Get the terminator yield op.
+YieldOp TaskOp::getYieldOp() {
+  return cast<YieldOp>(getBody().front().getTerminator());
+}
+
+bool TaskOp::isLivein(Value value) {
+  auto liveins = Liveness(*this).getLiveIn(&(*this).getBody().front());
+  return liveins.count(value);
+}
+
+SmallVector<Value> TaskOp::getLiveins() {
+  auto liveins = Liveness(*this).getLiveIn(&(*this).getBody().front());
+  return {liveins.begin(), liveins.end()};
+}
+
+SmallVector<Operation *> TaskOp::getLiveinUsers(Value livein) {
+  assert(isLivein(livein) && "invalid livein");
+  auto users = llvm::make_filter_range(livein.getUsers(), [&](Operation *user) {
+    return (*this)->isAncestor(user);
+  });
+  return {users.begin(), users.end()};
 }
 
 //===----------------------------------------------------------------------===//
